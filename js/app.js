be79b7cc679a80912ba4640ddcd63869c56c0307
Almuhammadi <muhammad.almuhammady@gmail.com>
2021-11-24 23:38:36 +0200

    smooth scrolling from js

--- js/app.js
+++ js/app.js
@@ -1,50 +1,43 @@
-let sections = document.querySelectorAll('.landing__container');
-
+let sections = document.querySelectorAll(".landing__container");
 // Create nav items with nav links and add them to nav bar
-let navBarList = document.getElementById('navbar__list');
+let navBarList = document.getElementById("navbar__list");
 for (let sectionIdx = 0; sectionIdx < sections.length; sectionIdx++) {
-
     let parent = sections[sectionIdx].parentNode;
-    let sectionName = parent.getAttribute('data-nav');
-
-    let sectionId = parent.getAttribute('id');
-    let navItem = document.createElement('li');
-    let navLink = document.createElement('a');
-    navLink.href = '#' + sectionId;
-    navLink.classList.add('menu__link');
+    let sectionName = parent.getAttribute("data-nav");
+    let sectionId = parent.getAttribute("id");
+    let navItem = document.createElement("li");
+    let navLink = document.createElement("a");
+    navLink.href = "#" + sectionId;
+    navLink.classList.add("menu__link");
     navLink.innerText = sectionName;
-
-    navLink.addEventListener('click', function (e) {
+    // scroll to section when nav link is clicked
+    navLink.addEventListener("click", function (e) {
         e.preventDefault();
-        let navText = this.innerText;
-        
-        // let parent = sections[sectionIdx].parentNode;
-        window.scrollTo(parent.getBoundingClientRect().top);
-    })
-
+        console.log("Hi");
+        let navText = "#" + this.href.split("#")[1];
+        let section = document.querySelector(navText);
+        section.scrollIntoView({
+            behavior: "smooth",
+        });
+    });
     navItem.appendChild(navLink);
-
     navBarList.appendChild(navItem);
 }
-
 // Sets the active section
-let activeSectionIdx = 0
+let activeSectionIdx = 0;
 let activeSection = sections.item(activeSectionIdx);
-window.addEventListener('scroll', function () {
+window.addEventListener("scroll", function () {
     for (let sectionIdx = 0; sectionIdx < sections.length; sectionIdx++) {
         let parent = sections[sectionIdx].parentNode;
         let scrollPosition = parent.getBoundingClientRect().top;
-
         if (scrollPosition <= 100 && scrollPosition >= 0) {
             let navItems = navBarList.childNodes;
-            activeSection.classList.remove('activeSection');
-            navItems[activeSectionIdx + 1].firstChild.classList.remove('activeNav');
-
+            activeSection.classList.remove("activeSection");
+            navItems[activeSectionIdx + 1].firstChild.classList.remove("activeNav");
             activeSection = parent;
             activeSectionIdx = sectionIdx;
-
-            activeSection.classList.add('activeSection');
-            navItems[activeSectionIdx + 1].firstChild.classList.add('activeNav');
+            activeSection.classList.add("activeSection");
+            navItems[activeSectionIdx + 1].firstChild.classList.add("activeNav");
         }
     }
 });
